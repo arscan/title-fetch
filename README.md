8bc82147bbacfb7778797362db6edf754c922903
Rob Scanlon <robscanlon@gmail.com>
2013-07-29 20:42:16 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Just a service that grabs the title of a page and returns it back in a json container
---
+++ index.js
@@ -0,0 +1,27 @@
+var http = require("http"),
+    superagent = require("superagent"),
+    express = require("express")
+    jsdom = require("jsdom")
+    app = express(),
+    port = process.env.PORT || 5000;
+
+app.get('/', function(request, response) {
+    console.log("Getting " + request.query.q);
+    jsdom.env(request.query.q, 
+
+        function(errors, window){
+            response.header('Content-Type', 'application/json');
+            response.header('Access-Control-Allow-Origin:', '*');
+            response.send({title: window.document.title});
+    });
+    // superagent.get(request.query.q).end(function(error,res){
+    //     response.send(res.text);
+    // });
+});
+
+
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
+
+
